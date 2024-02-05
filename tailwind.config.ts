1f654c71a24aa696826b88b439dba7ff2413bbd3
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-02-05 14:53:52 -0300

    config: initialzing project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        brand: {
+          green: {
+            main: "#D1E500",
+            light: "#F4FF80",
+          },
+          light: "#FAFAFA",
+        },
+        dark: {
+          main: "#1A1A1A",
+          secondary: "#2e2f36",
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
