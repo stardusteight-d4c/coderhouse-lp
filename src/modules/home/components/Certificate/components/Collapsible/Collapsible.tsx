804d3dce5556f038485f301d97e1a94ac6676206
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-02-06 10:56:54 -0300

    chore: create Collapsible animation box

--- src/modules/home/components/Certificate/components/Collapsible/Collapsible.tsx
+++ src/modules/home/components/Certificate/components/Collapsible/Collapsible.tsx
@@ -47,38 +47,63 @@ export const Collapsible: React.FC<CollapsibleProps> = ({
     }
   }
 
+  function handleCollapsibleAnimation(title: string) {
+    if (active === title) {
+      return {
+        transform: "scaleY(1)",
+        opacity: 1,
+        transition: "all 0.3s ease 0s",
+        transformOrigin: "0% 0% 0px",
+        height: "100px",
+      }
+    } else {
+      return {
+        transform: "scaleY(0)",
+        opacity: 0,
+        transition: "all 0.3s ease 0s",
+        transformOrigin: "0% 0% 0px",
+        height: "0px",
+      }
+    }
+  }
+
   return (
     <div>
       {data.map((item, index) => (
-        <div key={index} className="h-full relative pb-[59px]">
+        <div
+          key={index}
+          className="h-full transition-all ease-in-out duration-300 relative pb-[59px]"
+        >
           <div
             className={
-              "w-[2px] h-full absolute top-4 left-[7px] " + handleConnect(index)
+              "w-[2px] transition-all ease-in-out duration-300 h-full absolute top-4 left-[7px] " +
+              handleConnect(index)
             }
           />
           <h3
             onClick={() => handleActiveItem(item.title)}
             className={
-              "flex items-center group gap-x-16 cursor-pointer text-3xl leading-[36px] font-medium " +
+              "flex items-center transition-all ease-in-out duration-300 group gap-x-16 cursor-pointer text-3xl leading-[36px] font-medium " +
               handleTitleColor(item.title)
             }
           >
             <div
               className={
-                "rounded-full max-w-[16px]  max-h-[16px] min-w-[16px] min-h-[16px] w-4 h-4 z-[100] bg-brand-green-light " +
+                "rounded-full max-w-[16px] transition-all ease-in-out duration-300 max-h-[16px] min-w-[16px] min-h-[16px] w-4 h-4 z-[100] bg-brand-green-light " +
                 handleConnect(index, true)
               }
             />
             {item.title}
           </h3>
-          {active === item.title && (
-            <div className="flex items-center gap-x-16">
-              <div className="max-w-[16px] min-w-[16px]" />
-              <span className="text-dark-low block pt-[46px] px-4">
-                {item.about}
-              </span>
-            </div>
-          )}
+          <div
+            style={handleCollapsibleAnimation(item.title)}
+            className="flex items-center gap-x-16"
+          >
+            <div className="max-w-[16px] min-w-[16px]" />
+            <span className="text-dark-low block pt-[46px] px-4">
+              {item.about}
+            </span>
+          </div>
         </div>
       ))}
     </div>
